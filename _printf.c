47db43918f6ea304426695ecdef15e71bfd23b2a
Erica Onyegwu <ericaalbert44@gmail.com>
2022-08-09 16:58:46 -0700

    Starter files

---
+++ _printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _printf - prints depends of the arguments.
+ * @format: s for string, c for char, d for decimals, i for integers,
+ * b for cast to binary, u for cast to unsigned decimal, o for print
+ * in octal, x for lowercase Hexadecimal, X for Uppercase Hexadecimal,
+ * p to print adresses
+ * Return: new string.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list elements;
+
+	va_start(elements, format);
+
+
+	va_end(elements);
+}
